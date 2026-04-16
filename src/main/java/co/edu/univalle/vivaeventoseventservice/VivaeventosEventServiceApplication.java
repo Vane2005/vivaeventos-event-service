fabd102d578564c427a21890f28bca4c7432ddb8
Vane2005 <vanessaduranmona@gmail.com>
2026-04-16 18:57:48 -0500

    Envia estructura base del proyecto

---
+++ src/main/java/co/edu/univalle/vivaeventoseventservice/VivaeventosEventServiceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.univalle.vivaeventoseventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaeventosEventServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VivaeventosEventServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/univalle/vivaeventoseventservice/VivaeventosEventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.univalle.vivaeventoseventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaeventosEventServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
